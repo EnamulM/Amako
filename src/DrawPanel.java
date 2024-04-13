9c18cbd462b5a6774adf51bbff5fdf6538204dba
Enamulm <enamulm@nycstudents.net>
2024-04-13 13:08:49 -0400

    Implemented MouseListener. Background doesn't work yet

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -2,36 +2,59 @@ import java.awt.*;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import javax.swing.*;
-import java.util.ArrayList;
-import javax.swing.JPanel;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-public class DrawPanel extends JPanel {
+public class DrawPanel extends JPanel implements MouseListener{
 
     private Rectangle button;
+    private Image background;
+
 
     public DrawPanel() {
         button = new Rectangle(565, 310, 805, 265);
+        addMouseListener(this);
+        try {
+            background = ImageIO.read(new File("OpeningBackGround.jpg"));
+        } catch (IOException e) {
+            // Handle exception
+        }
 
     }
 
     protected void paintComponent(Graphics g) {
-        Image img = null;
-        try {
-            img = ImageIO.read(new File("OpeningBackGround.jpg"));
-        } catch (IOException e) {
-            //do something
-        }
         super.paintComponent(g);
         int x = 50;
         int y = 10;
         g.setFont(new Font("Times New Roman", Font.BOLD, 200));
         g.drawString("AMAKO", 575, 500);
         g.drawRect((int) button.getX(), (int) button.getY(), (int) button.getWidth(), (int) button.getHeight());
-        g.drawImage(img, 500, 500, null);
+        g.drawImage(background, 500, 500, null);
     }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (button.contains(e.getPoint())) {
+            try {
+                background = ImageIO.read(new File("OpeningBackground.jpg"));
+                repaint();
+            } catch (IOException ex) {
+                // Handle exception
+                System.err.println("Error loading background image: " + ex.getMessage());
+            }
+        }
+    }
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
+
 }
 
