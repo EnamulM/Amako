aed80b5990c2e5fd253dddeaac0e8c5cd47177a2
Enamulm <enamulm@nycstudents.net>
2024-06-02 09:19:35 -0400

    Started Jump Animation, doesn't work yet

--- src/Batman.java
+++ src/Batman.java
@@ -12,26 +12,36 @@ public class Batman extends JPanel implements KeyListener {
     private int damage;
     private int x;
     private int y;
+    private int startY;
     private BufferedImage background;
     private BufferedImage idleFrame;
 
     private ArrayList<BufferedImage> walkFrames;
-    private static Image bWalk;
+    private ArrayList<BufferedImage> jumpFrames;
 
     private int currentFrame;
     private long startTime;
     private boolean isMoving;
+    private boolean isJumping;
+    private int jumpHeight = 30;
+    private int jumpSpeed = 5;
+    private int jumpFrameCount;
 
     public Batman(int health, int damage, int x, int y) {
         this.health = health;
         this.damage = damage;
         this.x = x;
         this.y = y;
+        this.startY = y;
         walkFrames = new ArrayList<>();
+        jumpFrames = new ArrayList<>();
         loadWalkFrames();
+        loadJumpFrames();
         currentFrame = 0;
         startTime = System.currentTimeMillis();
         isMoving = false;
+        isJumping = false;
+        jumpFrameCount = 0;
 
         try {
             background = ImageIO.read(new File("BackgroundImages/OpeningBackGround.jpg"));
@@ -40,7 +50,6 @@ public class Batman extends JPanel implements KeyListener {
         }
         try {
             idleFrame = ImageIO.read(new File("Batman/Idle.png"));
-
         } catch (IOException e) {
             System.out.println("Error Loading Image! Sorry! Idle");
         }
@@ -49,60 +58,6 @@ public class Batman extends JPanel implements KeyListener {
         this.addKeyListener(this);
     }
 
-
-    public void bWalk(Graphics g) throws InterruptedException {
-        try {
-            background = ImageIO.read(new File("OpeningBackGround.jpg"));
-        } catch (IOException e) {
-            System.out.println("Error Loading Image! Sorry! (Error Here)");
-        }
-        Graphics2D g2d = (Graphics2D) g;
-        try {
-            bWalk = ImageIO.read(new File("Batman/bIdle.png"));
-        } catch (IOException e) {
-            System.out.println("Error Loading Image! Sorry!");
-        }
-        g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
-        g2d.drawImage(bWalk, 0, 0, 500, 500, null);
-        try {
-            bWalk = ImageIO.read(new File("Batman/Walk.png"));
-        } catch (IOException e) {
-            System.out.println("Error Loading Image! Sorry!");
-        }
-        g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
-        g2d.drawImage(bWalk, 20, 0, 500, 500, null);
-        try {
-            bWalk = ImageIO.read(new File("Batman/Walk2.png"));
-        } catch (IOException e) {
-            System.out.println("Error Loading Image! Sorry!");
-        }
-        g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
-        g2d.drawImage(bWalk, 40, 0, 500, 500, null);
-        Thread.sleep(500);
-        try {
-            bWalk = ImageIO.read(new File("Batman/Walk3.png"));
-        } catch (IOException e) {
-            System.out.println("Error Loading Image! Sorry!");
-        }
-        g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
-        g2d.drawImage(bWalk, 80, 0, 500, 500, null);
-        Thread.sleep(500);
-        try {
-            bWalk = ImageIO.read(new File("Batman/Walk4.png"));
-        } catch (IOException e) {
-            System.out.println("Error Loading Image! Sorry!");
-        }
-        g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
-        g2d.drawImage(bWalk, 160, 0, 500, 500, null);
-        Thread.sleep(500);
-        try {
-            bWalk = ImageIO.read(new File("Batman/Walk5.png"));
-        } catch (IOException e) {
-            System.out.println("Error Loading Image! Sorry!");
-        }
-        g2d.drawImage(bWalk, 200, 0, 500, 500, null);
-        Thread.sleep(500);
-    }
     private void loadWalkFrames() {
         for (int i = 1; i <= 5; i++) {
             String file = "Batman/Walk" + i + ".png";
@@ -115,9 +70,21 @@ public class Batman extends JPanel implements KeyListener {
         }
     }
 
-    private void nextFrame() {
+    private void loadJumpFrames() {
+        for (int i = 1; i <= 5; i++) {
+            String file = "Batman/Jump" + i + ".png";
+            try {
+                BufferedImage frame = ImageIO.read(new File(file));
+                jumpFrames.add(frame);
+            } catch (IOException e) {
+                System.out.println("Error Loading Image! Sorry!");
+            }
+        }
+    }
+
+    private void nextFrame(ArrayList<BufferedImage> frames) {
         currentFrame++;
-        if (currentFrame == walkFrames.size()) {
+        if (currentFrame == frames.size()) {
             currentFrame = 0;
         }
     }
@@ -127,12 +94,30 @@ public class Batman extends JPanel implements KeyListener {
         super.paintComponent(g);
         g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
 
-        if (isMoving) {
+        if (isJumping) {
+            long currentTime = System.currentTimeMillis();
+            double timeElapsed = (double) (currentTime - startTime) / 1000;
+            if (timeElapsed > 0.1) {
+                startTime = System.currentTimeMillis();
+                nextFrame(jumpFrames);
+                jumpFrameCount++;
+                if (jumpFrameCount < jumpFrames.size() / 2) {
+                    y -= jumpSpeed;
+                } else if (jumpFrameCount < jumpFrames.size()) {
+                    y += jumpSpeed;
+                } else {
+                    y = startY;
+                    isJumping = false;
+                    jumpFrameCount = 0;
+                }
+            }
+            g.drawImage(jumpFrames.get(currentFrame), x, y, 100, 100, null);
+        } else if (isMoving) {
             long currentTime = System.currentTimeMillis();
             double timeElapsed = (double) (currentTime - startTime) / 1000;
             if (timeElapsed > 0.3) {
                 startTime = System.currentTimeMillis();
-                nextFrame();
+                nextFrame(walkFrames);
             }
             g.drawImage(walkFrames.get(currentFrame), x, y, 100, 100, null);
         } else {
@@ -145,8 +130,11 @@ public class Batman extends JPanel implements KeyListener {
         int key = e.getKeyCode();
         switch (key) {
             case KeyEvent.VK_W:
-                y -= 10;
-                isMoving = true;
+                if (!isJumping) {
+                    isJumping = true;
+                    currentFrame = 0;
+                    startTime = System.currentTimeMillis();
+                }
                 break;
             case KeyEvent.VK_S:
                 y += 10;
@@ -173,4 +161,3 @@ public class Batman extends JPanel implements KeyListener {
     public void keyTyped(KeyEvent e) {
     }
 }
-
