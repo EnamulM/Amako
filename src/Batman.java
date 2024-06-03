e7b4bead38bfbe02304f05766f91ac4283a19ac3
Enamulm <enamulm@nycstudents.net>
2024-06-03 17:38:28 -0400

    Finished kick method using the same process as the walk, jump and punch method

--- src/Batman.java
+++ src/Batman.java
@@ -19,10 +19,13 @@ public class Batman extends JPanel implements KeyListener {
     private ArrayList<BufferedImage> walkFrames;
     private ArrayList<BufferedImage> jumpFrames;
     private ArrayList<BufferedImage> punchFrames;
+    private ArrayList<BufferedImage> kickFrames;
 
     private int currentWalkFrame;
     private int currentJumpFrame;
     private int currentPunchFrame;
+    private int currentKickFrame;
+
     private boolean isMoving;
     private boolean isJumping;
     private boolean isPunching;
@@ -31,31 +34,32 @@ public class Batman extends JPanel implements KeyListener {
     private Timer jumpTimer;
     private Timer walkTimer;
     private Timer punchTimer;
+    private Timer kickTimer;
     private int jumpFrameCount;
 
     public Batman(int health, int damage, int x, int y) {
         this.health = health;
         this.damage = damage;
         this.x = x;
-        this.y = y;
-        this.startY = y;
-
+        this.y = 400;
+        this.startY = 400;
         walkFrames = new ArrayList<>();
         jumpFrames = new ArrayList<>();
         punchFrames = new ArrayList<>();
-
+        kickFrames = new ArrayList<>();
         loadWalkFrames();
         loadJumpFrames();
         loadPunchFrames();
+        loadKickFrames();
         currentWalkFrame = 0;
         currentJumpFrame = 0;
         currentPunchFrame = 0;
-
+        currentKickFrame = 0;
         isMoving = false;
         isJumping = false;
         isPunching = false;
+        isKicking = false;
         jumpFrameCount = 0;
-
         try {
             background = ImageIO.read(new File("BackgroundImages/OpeningBackGround.jpg"));
         } catch (IOException e) {
@@ -101,6 +105,18 @@ public class Batman extends JPanel implements KeyListener {
             try {
                 BufferedImage frame = ImageIO.read(new File(file));
                 punchFrames.add(frame);
+            } catch (IOException e) {
+                System.out.println("Error Loading Image! Sorry!");
+            }
+        }
+    }
+
+    private void loadKickFrames() {
+        for (int i = 1; i <= 2; i++) {
+            String file = "Batman/Kick" + i + ".png";
+            try {
+                BufferedImage frame = ImageIO.read(new File(file));
+                kickFrames.add(frame);
             } catch (IOException e) {
                 System.out.println("Error Loading Image! Sorry!");
             }
@@ -171,6 +187,28 @@ public class Batman extends JPanel implements KeyListener {
         punchTimer.start();
     }
 
+    private void kick() {
+        if (isKicking) {
+            return;
+        }
+        isKicking = true;
+        currentKickFrame = 0;
+        kickTimer = new Timer(150, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (currentKickFrame < kickFrames.size()) {
+                    repaint();
+                    currentKickFrame++;
+                } else {
+                    ((Timer) e.getSource()).stop();
+                    isKicking = false;
+                    repaint();
+                }
+            }
+        });
+        kickTimer.start();
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -180,6 +218,8 @@ public class Batman extends JPanel implements KeyListener {
             g.drawImage(jumpFrames.get(currentJumpFrame), x, y, 100, 100, null);
         } else if (isPunching && currentPunchFrame < punchFrames.size()) {
             g.drawImage(punchFrames.get(currentPunchFrame), x, y, 100, 100, null);
+        } else if (isKicking && currentKickFrame < kickFrames.size()) {
+            g.drawImage(kickFrames.get(currentKickFrame), x, y, 100, 100, null);
         } else if (isMoving) {
             g.drawImage(walkFrames.get(currentWalkFrame), x, y, 100, 100, null);
         } else {
@@ -211,6 +251,9 @@ public class Batman extends JPanel implements KeyListener {
                 break;
             case KeyEvent.VK_E:
                 punch();
+                break;
+            case KeyEvent.VK_R:
+                kick();
                 break;
         }
         repaint();
